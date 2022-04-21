b2f14f4bb06cc07b3fc39f5c168cc34eecc00744
Ilkka Pättikangas <ilkka.pattikangas@gmail.com>
2022-04-21 14:34:20 +0300

    Change loading spinner name to preloader

--- src/componentIcons.ts
+++ src/componentIcons.ts
@@ -1,7 +1,7 @@
 import radioButton from './componentIcons/icon-radio-button.svg';
 import radioButtonLarge from './componentIcons/icon-radio-button-large.svg';
 import toggle from './componentIcons/icon-toggle.svg';
-import loadingSpinner from './componentIcons/icon-loading-spinner.svg';
+import preloader from './componentIcons/icon-preloader.svg';
 
 export type ComponentIconKeys = keyof typeof componentIcons;
 
@@ -9,5 +9,5 @@ export const componentIcons = {
   radioButton,
   radioButtonLarge,
   toggle,
-  loadingSpinner
+  preloader
 };
