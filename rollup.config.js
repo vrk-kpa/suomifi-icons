15609132e14d65ca4b41fafb94c36023804adefe
LJKaski <joonas.kaski@gofore.com>
2022-09-09 15:06:40 +0300

    create rollup config to build icon types individually

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,53 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-const packageJson = require("./package.json");
+import svgr from "@svgr/rollup";
+const illustrativeConfig = require("./svgo.config-illustrative.js");
+const baseConfig = require("./svgo.config-base.js");
+const componentConfig = require("./svgo.config-component.js");
+const defaultConfig = require("./svgo.config-default.js");
 
-export default {
-  // entry points
-  input: "./src/index.ts",
-  // output files
-  output: [
-    {
-      file: packageJson.module,
-      format: "esm", // ES Modules
+const iconTypes = ["base", "illustrative", "component"]; // add doctype and logo
+
+const resolveConfig = (iconType) => {
+  switch (iconType) {
+    case "base":
+      return baseConfig;
+    case "illustrative":
+      return illustrativeConfig;
+    case "component":
+      return componentConfig;
+    default:
+      return defaultConfig;
+  }
+};
+
+const buildIcons = (iconType) => {
+  return {
+    input: `src/${iconType}Icons/index.ts`,
+    output: {
+      dir: `dist/${iconType}Icons`,
+      format: "esm",
       sourcemap: true,
     },
-  ],
-  // Plugins array
-  plugins: [
-    peerDepsExternal(), // prevents bundling peerDependencies
-    resolve(), // resolves package entrypoints
-    typescript({ useTsconfigDeclarationDir: true }), // typescript
-    commonjs(),
-  ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      svgr({
+        typescript: true,
+        icon: true,
+        svgoConfig: resolveConfig(iconType),
+      }),
+      commonjs(),
+      typescript(),
+    ],
+  };
 };
+
+export default [
+  buildIcons("base"),
+  buildIcons("illustrative"),
+  buildIcons("component"),
+  buildIcons("logo"),
+  buildIcons("doctype"),
+];
