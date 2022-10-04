83d04f2c7bf98240cadbcc196d024e237c0d8fa8
Joonas Kaski <joonas.kaski@gofore.com>
2022-10-04 13:01:41 +0300

    add aria, frank utils and common styles

---
+++ src/utils/styles.tsx
@@ -0,0 +1,31 @@
+import { css } from 'styled-components';
+
+export const iconStyles = ({
+  color,
+  fill
+}: {
+  color?: string;
+  fill?: string;
+}) => {
+  const resolvedColor = fill ?? color ?? 'currentColor';
+  return css`
+    vertical-align: baseline;
+    &.fi-icon {
+      display: inline-block;
+    }
+    .fi-icon-base-fill {
+      fill: ${resolvedColor};
+    }
+
+    .fi-icon-base-stroke {
+      stroke: ${resolvedColor};
+    }
+
+    &.fi-icon--cursor-pointer {
+      cursor: pointer;
+      & * {
+        cursor: inherit;
+      }
+    }
+  `;
+};
