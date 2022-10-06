8891516277d9bd2f6a9359f3f1e83150bff6e7e6
Joonas Kaski <joonas.kaski@gofore.com>
2022-10-06 16:16:33 +0300

    remove redundant files

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { Horizontal } from "./logoIcons";
-export { Archive } from "./illustrativeIcons";
-export { Doc } from "./doctypeIcons";
-export { Preloader } from "./componentIcons";
-export { Alert } from "./baseIcons";
