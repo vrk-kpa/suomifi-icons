517ba65e192e07edba98ecf598ffde8bf73c912d
Joonas Kaski <joonas.kaski@gofore.com>
2022-12-16 15:43:37 +0200

    update rollup config to use all components as input points and use named exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import svgr from '@svgr/rollup';
+import glob from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 const illustrativeConfig = require('./svgo.config-illustrative.js');
 const baseConfig = require('./svgo.config-base.js');
 const componentConfig = require('./svgo.config-component.js');
@@ -23,25 +26,42 @@ const resolveConfig = (iconType) => {
 
 const buildIcons = (iconType) => {
   return {
-    input: `src/${iconType}Icons/index.ts`,
-    output: {
-      dir: `dist/${iconType}Icons`,
-      format: 'esm',
-      sourcemap: true,
-      preserveModules: true
-    },
+    input: Object.fromEntries(
+      glob.sync(`src/${iconType}Icons/*.*`).map((file) => [
+        // This remove `src/` as well as the file extension from each file, so e.g.
+        // src/nested/foo.js becomes nested/foo
+        path.relative(
+          'src',
+          file.slice(0, file.length - path.extname(file).length)
+        ),
+        // This expands the relative paths to absolute paths, so e.g.
+        // src/nested/foo becomes /project/src/nested/foo.js
+        fileURLToPath(new URL(file, import.meta.url))
+      ])
+    ),
+    output: [
+      {
+        dir: `dist/${iconType}Icons/esm`,
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        dir: `dist/${iconType}Icons/cjs`,
+        format: 'cjs',
+        sourcemap: true
+      }
+    ],
     plugins: [
       peerDepsExternal(),
       resolve(),
       svgr({
         typescript: true,
         icon: true,
-        svgoConfig: resolveConfig(iconType)
+        svgoConfig: resolveConfig(iconType),
+        exportType: 'named'
       }),
       commonjs(),
-      typescript({
-        useTsconfigDeclarationDir: true
-      })
+      typescript({ useTsconfigDeclarationDir: true })
     ]
   };
 };
