8abaca05722d928133625a6342efb80fad415a0a
Joonas Kaski <joonas.kaski@gofore.com>
2023-03-14 09:43:19 +0200

    remove redundant svgo logic from rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,6 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-const illustrativeConfig = require('./svgo.config-illustrative.js');
-const baseConfig = require('./svgo.config-base.js');
-const componentConfig = require('./svgo.config-component.js');
-const defaultConfig = require('./svgo.config-default.js');
-
-const resolveConfig = (iconType) => {
-  switch (iconType) {
-    case 'base':
-      return baseConfig;
-    case 'illustrative':
-      return illustrativeConfig;
-    case 'component':
-      return componentConfig;
-    default:
-      return defaultConfig;
-  }
-};
 
 const buildIcons = () => {
   return {
